5ec7d4856353a356521124cb132003a7fd763338
tanyang <122074809@qq.com>
2017-07-04 10:04:35 +0800

    Initial Commit

---
+++ TYCyclePagerViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TYCyclePagerViewDemo
+//
+//  Created by tany on 2017/7/4.
+//  Copyright © 2017年 tany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TYCyclePagerViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TYCyclePagerViewDemo
+//
+//  Created by tany on 2017/7/4.
+//  Copyright © 2017年 tany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
