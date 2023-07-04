9be37387beff43d5818de88179d48f8c43704d43
wyon <wyon.wang@gmail.com>
2023-07-04 11:05:05 +0800

    修改配置
    Fix：修改版本配置、头文件导入配置

--- TYCyclePagerViewDemo/TYCyclePagerView/TYCyclePagerView.h
+++ TYCyclePagerViewDemo/TYCyclePagerView/TYCyclePagerView.h
@@ -7,14 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
-#if __has_include(<TYCyclePagerView/TYCyclePagerView.h>)
-#import <TYCyclePagerView/TYCyclePagerTransformLayout.h>
-#else
 #import "TYCyclePagerTransformLayout.h"
-#endif
-#if __has_include(<TYCyclePagerView/TYPageControl.h>)
-#import <TYCyclePagerView/TYPageControl.h>
-#endif
+#import "TYPageControl.h"
+//#if __has_include(<TYCyclePagerView/TYCyclePagerView.h>)
+//#import <TYCyclePagerView/TYCyclePagerTransformLayout.h>
+//#else
+//#import "TYCyclePagerTransformLayout.h"
+//#endif
+//#if __has_include(<TYCyclePagerView/TYPageControl.h>)
+//#import <TYCyclePagerView/TYPageControl.h>
+//#endif
 
 NS_ASSUME_NONNULL_BEGIN
 
